5689f400c92d6912fa49fe373c180081fb624196
joao front <joao.saousa@tangotech.com.br>
2020-04-01 15:59:43 -0300

    inicio projeto

---
+++ script/controler/CalcContrlller.js
@@ -0,0 +1,18 @@
+class CalcControler{
+    constructor(){
+        this._displayCalc ="0";
+        this._dataAtual
+    }
+    get _displayCalc(){
+        return this._displayCalc
+    }
+    set _displayCalc(valor){
+        this._displayCalc = valor;
+    }
+    get _dataAtual(){
+        return this._dataAtual
+    }
+    set _dataAtual(valor){
+        this._dataAtual = valor;
+    }
+}
